fd0c8cef01a46d5d924dfa5e47e917f375667401
Emil Guluzade <emilquluzade2001@gmail.com>
2023-01-06 16:25:28 +0400

    small update

--- ChessMobileBE/Models/DTOs/Responses/FinishMatchResponse.cs
+++ ChessMobileBE/Models/DTOs/Responses/FinishMatchResponse.cs
@@ -1,12 +1,14 @@
-﻿namespace ChessMobileBE.Models.DTOs.Responses
+﻿using ChessMobileBE.Helpers;
+using ChessMobileBE.Models.DBModels;
+
+namespace ChessMobileBE.Models.DTOs.Responses
 {
     public class FinishMatchResponse
     {
         public string RoomId { get; set; }
-        public string UserId { get; set; }
-        public string OpponendId { get; set; }
-        public bool Victory { get; set; }
+        public WinState VictoryState { get; set; }
         public int UserCorrectMoveCount { get; set; }
         public int OpponentCorrectMoveCount { get; set; }
+        public User UserModel { get; set; }
     }
 }
